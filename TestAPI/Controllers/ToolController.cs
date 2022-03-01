294e179dbd9aa74f79d67c68a3fa3bb2e3124daf
Shadrack Kiprotich <39646967+shadrackkiprotich@users.noreply.github.com>
2022-03-01 23:32:09 +0300

    Add project files.

---
+++ TestAPI/Controllers/ToolController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TestAPI.Models;
+
+namespace TestAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class ToolController :ControllerBase
+    {
+      /*  public IActionResult Index()
+        {
+            return View();
+
+
+        }*/
+        [HttpPost]
+        public HttpResponseMessage AddTool( Tool tool)
+        {
+            Console.WriteLine("Added Model");
+            HttpResponseMessage response = new HttpResponseMessage( System.Net.HttpStatusCode.OK);
+            return response;
+        }
+
+
+        [HttpPut]
+        public HttpResponseMessage EditTool(String  toolId)
+        {
+            Console.WriteLine("Added Model");
+            HttpResponseMessage response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            return response;
+        }
+
+
+        [HttpDelete]
+        public HttpResponseMessage DeleteTool (String toolID)
+        {
+            Console.WriteLine("Deleted Model");
+            HttpResponseMessage response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            return response;
+        }
+
+
+
+
+
+
+    }
+}
